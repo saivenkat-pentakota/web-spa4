3b6539a3fb9f89588c1552e533574543837ef2b0
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2024-03-09 20:33:07 +0530

    created service component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,14 +1,132 @@
 import React from "react";
 
-import './Services.css';
+import "./Services.css";
 
-function Services(){
-    return(
-        <div className="services-container">
-
+function Services() {
+  return (
+    <div id="services">
+      <div className="container">
+        <div className="section-header">
+          <h2>Our Services</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            viverra at massa sit amet ultricies. Nullam consequat, mauris non
+            interdum cursus
+          </p>
         </div>
-    );
-
+        <div className="row">
+          <div className="col-md-6 col-lg-4">
+            <div className="service-col">
+              <div className="service-icon">
+                <i className="fa fa-television"></i>
+              </div>
+              <h3>Web Design</h3>
+              <div className="service-detail">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                viverra at massa sit amet ultricies. Nullam consequat mauris non
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <div className="service-col">
+              <div className="service-icon">
+                <i className="fa fa-laptop"></i>{" "}
+              </div>
+              <h3>Web Development</h3>
+              <div className="service-detail">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                viverra at massa sit amet ultricies. Nullam consequat mauris non
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <div className="service-col">
+              <div className="service-icon">
+                <i className="fa fa-cog"></i>{" "}
+              </div>
+              <h3>Theme Development</h3>
+              <div className="service-detail">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                viverra at massa sit amet ultricies. Nullam consequat mauris non
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <div className="service-col">
+              <div className="service-icon">
+                <i className="fa fa-cogs"></i>{" "}
+              </div>
+              <h3>Game Development</h3>
+              <div className="service-detail">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                viverra at massa sit amet ultricies. Nullam consequat mauris non
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <div className="service-col">
+              <div className="service-icon">
+                <i className="fa fa-android"></i>{" "}
+              </div>
+              <h3>Apps Development</h3>
+              <div className="service-detail">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                viverra at massa sit amet ultricies. Nullam consequat mauris non
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <div className="service-col">
+              <div className="service-icon">
+                <i className="fa fa-television"></i>{" "}
+              </div>
+              <h3>Desktop Application</h3>
+              <div className="service-detail">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                viverra at massa sit amet ultricies. Nullam consequat mauris non
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <div className="service-col">
+              <div className="service-icon">
+                <i className="fa fa-wordpress"></i>{" "}
+              </div>
+              <h3>WordPress Themes</h3>
+              <div className="service-detail">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                viverra at massa sit amet ultricies. Nullam consequat mauris non
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <div className="service-col">
+              <div className="service-icon">
+                <i className="fa fa-cog"></i>{" "}
+              </div>
+              <h3>WordPress Plugins</h3>
+              <div className="service-detail">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                viverra at massa sit amet ultricies. Nullam consequat mauris non
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4">
+            <div className="service-col">
+              <div className="service-icon">
+                <i className="fa fa-phone"></i>{" "}
+              </div>
+              <h3>Support & IT</h3>
+              <div className="service-detail">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+                viverra at massa sit amet ultricies. Nullam consequat mauris non
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
